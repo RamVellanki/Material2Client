85347ea56eb6eefe2b4880780fd69e61a34cc97e
unknown <j1009063@J1009063W8LT.jda.corp.local>
2016-02-27 10:48:59 +0530

    initial minor changes to toolbar

--- components/toolbar/toolbar.ts
+++ components/toolbar/toolbar.ts
@@ -1,10 +1,10 @@
 import {Component, ViewEncapsulation, ChangeDetectionStrategy} from 'angular2/core';
 
 @Component({
-  selector: 'md-card',
-  templateUrl: './components/card/card.html',
-  styleUrls: ['./components/card/card.css'],
+  selector: 'md-toolbar',
+  templateUrl: './components/toolbar/toolbar.html',
+  styleUrls: ['./components/toolbar/toolbar.css'],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MdCard {}
+export class MdToolbar {}
